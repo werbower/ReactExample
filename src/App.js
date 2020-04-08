c1e3ee02eba5b92c14154dbb5e463b9759da4d4d
werbower <werbower@gmail.com>
2020-04-08 10:06:34 +0300

    day 1. nested component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function MovieItem(props) {
-  console.log('Movie Item, props ', props);
+const dataObj = {
+  title: 'Super title',
+  vote_average: 9.456,
+  image: `https://images.pexels.com/photos/3757147/pexels-photo-3757147.jpeg?cs=srgb&dl=pexels-3757147.jpg&fm=jpg`,
+}
+
+function Image (props) {
+  const {src, alt} = props;
   return (
-    <div>
-      <p>{props.title}</p>
-      <p>{props.vote_average}</p>
-    </div>
+    <img src={src} alt={alt} style={{width: '500px', height: 'auto'}}/>
   );
+}
+
+class MovieItem extends React.Component {
+  render() {
+    const {data: {title, vote_average, image: src}} = this.props;
+    return (
+      <div>
+      <p>{title}</p>
+      <p>{vote_average}</p>
+      <Image src={src} alt={title}/>
+    </div>
+    );
+  }
 }
 
 function App() {
   return (
     <div>
-      <MovieItem title='Title new' vote_average={13.5} />
+      <MovieItem data={dataObj} />
     </div>
   );
 }
